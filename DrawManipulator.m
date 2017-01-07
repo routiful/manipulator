b4226b9ad9d5c9eadb91592d59e2b01d216d895d
routiful <routiful@gmail.com>
2017-01-08 03:18:43 +0900

    first commit

--- DrawManipulator.m
+++ DrawManipulator.m
@@ -2,28 +2,28 @@ clf;
 global uLINK % Refer global variable uLINK   
              
 for i=1:8
-    tmp = uLINK(2).R * uLINK(2).joint_vertex(i,:)';
-    uLINK(2).joint_vertex(i,:) = tmp';
-    tmp = uLINK(2).R * uLINK(2).link_vertex(i,:)';
-    uLINK(2).link_vertex(i,:) = tmp';
+%     tmp = uLINK(2).p + uLINK(2).joint_vertex(i,:)';
+%     uLINK(2).joint_vertex(i,:) = tmp';
+%     tmp = uLINK(2).p + uLINK(2).link_vertex(i,:)';
+%     uLINK(2).link_vertex(i,:) = tmp';
     
-    tmp = uLINK(3).R * uLINK(3).joint_vertex(i,:)';
+    tmp = Rodrigues([1 0 0]', 90) * ([0 0 1]' + uLINK(3).joint_vertex(i,:)');
     uLINK(3).joint_vertex(i,:) = tmp';
-    tmp = uLINK(3).R * uLINK(3).link_vertex(i,:)';
+    tmp = Rodrigues([1 0 0]', 90) * ([0 0 1]' + uLINK(3).link_vertex(i,:)');
     uLINK(3).link_vertex(i,:) = tmp';
     
-    tmp = uLINK(4).R * uLINK(4).joint_vertex(i,:)';
-    uLINK(4).joint_vertex(i,:) = tmp';
-    tmp = uLINK(4).R * uLINK(4).link_vertex(i,:)';
-    uLINK(4).link_vertex(i,:) = tmp';
+%     tmp = uLINK(4).p + uLINK(4).joint_vertex(i,:)';
+%     uLINK(4).joint_vertex(i,:) = tmp';
+%     tmp = uLINK(4).p + uLINK(4).link_vertex(i,:)';
+%     uLINK(4).link_vertex(i,:) = tmp';
 end   
 
-patch('Vertices', uLINK(2).joint_vertex, 'faces', uLINK(2).face, 'Facecolor', [0.8 1 0.8]);
-patch('Vertices', uLINK(2).link_vertex,  'faces', uLINK(2).face, 'Facecolor', [1 1 1]);
+% patch('Vertices', uLINK(2).joint_vertex, 'faces', uLINK(2).face, 'Facecolor', [0.8 1 0.8]);
+% patch('Vertices', uLINK(2).link_vertex,  'faces', uLINK(2).face, 'Facecolor', [1 1 1]);
 patch('Vertices', uLINK(3).joint_vertex, 'faces', uLINK(3).face, 'Facecolor', [0.8 1 0.8]);
 patch('Vertices', uLINK(3).link_vertex,  'faces', uLINK(3).face, 'Facecolor', [1 1 1]);
-patch('Vertices', uLINK(4).joint_vertex, 'faces', uLINK(4).face, 'Facecolor', [0.8 1 0.8]);
-patch('Vertices', uLINK(4).link_vertex,  'faces', uLINK(4).face, 'Facecolor', [1 1 1]);
+% patch('Vertices', uLINK(4).joint_vertex, 'faces', uLINK(4).face, 'Facecolor', [0.8 1 0.8]);
+% patch('Vertices', uLINK(4).link_vertex,  'faces', uLINK(4).face, 'Facecolor', [1 1 1]);
 
 axis([-10 10 -10 10 -10 10]);
 grid on;
